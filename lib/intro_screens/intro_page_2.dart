a81bb3e783dcb21a846fcbae559cdd8985e99f1f
gabrielamflorencio <gabriela_marques_florencio@hotmail.com>
2024-03-21 21:11:14 -0300

    <feat> introduction screens

---
+++ lib/intro_screens/intro_page_2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IntroPage2 extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepPurple[100],
+      child: Center(
+          child: Column(
+            children: [
+              Text('Página 2'),
+              //put images and cool stuff here
+            ],
+          )
+      ),
+    );
+  }
+}
