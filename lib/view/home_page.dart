480e16f5f50380747032ee4f4826f5d1dba9f032
gabrielamflorencio <gabriela_marques_florencio@hotmail.com>
2024-03-25 20:09:32 -0300

    <feat> bottom nav bar

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super (key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +17,45 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(),
       body: Center(
         child: Text('HOME PAGE'),
-      )
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: 15.0,
+              vertical: 20.0
+          ),
+          child: GNav(
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey.shade800,
+            gap: 8,
+            onTabChange: (index){
+              print(index);
+            },
+            padding: EdgeInsets.all(16),
+            tabs: const [
+              GButton(
+                  icon: Icons.password_rounded,
+                  text: 'Senha',
+              ),
+              GButton(
+                  icon: Icons.message_rounded,
+                  text: 'Texto',
+              ),
+              GButton(
+                  icon: Icons.phonelink_ring_rounded,
+                  text: 'Telefone',
+              ),
+              GButton(
+                  icon: Icons.link_rounded,
+                  text: 'Site',
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
