e27ac0d39679242db6c038cdd127ce2f8a90557e
gabrielamflorencio <107224477+MarianaCardoso88@users.noreply.github.com>
2024-04-29 21:08:14 -0300

    ajuste da formatação do texto

--- lib/controller/apiAccess.dart
+++ lib/controller/apiAccess.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 Future <Map<String, dynamic>> passwordAnalysis (String password) async {
   String apiUrl = 'http://localhost:8000/check_password?password=$password';
   var response = await http.post(Uri.parse(apiUrl));
-  var resultado = jsonDecode(response.body);
+  var resultado = json.decode(utf8.decode(response.bodyBytes));
 
   return resultado;
 }
@@ -13,7 +13,7 @@ Future <Map<String, dynamic>> passwordAnalysis (String password) async {
 Future <Map<String, dynamic>> messageAnalysis (String message) async {
   String apiUrl = 'http://localhost:8000/check_sms?phone=19981450917&sms=$message';
   var response = await http.post(Uri.parse(apiUrl));
-  var resultado = jsonDecode(response.body);
+  var resultado = json.decode(utf8.decode(response.bodyBytes));
 
   return resultado;
 }
@@ -21,7 +21,7 @@ Future <Map<String, dynamic>> messageAnalysis (String message) async {
 Future <Map<String, dynamic>> callAnalysis (String phone) async {
   String apiUrl = 'http://localhost:8000/check_number?phone=$phone';
   var response = await http.post(Uri.parse(apiUrl));
-  var resultado = jsonDecode(response.body);
+  var resultado = json.decode(utf8.decode(response.bodyBytes));
 
   return resultado;
 }
@@ -29,7 +29,7 @@ Future <Map<String, dynamic>> callAnalysis (String phone) async {
 Future <Map<String, dynamic>> urlAnalysis (String url) async {
   String apiUrl = 'http://localhost:8000/check_url?url=$url';
   var response = await http.post(Uri.parse(apiUrl));
-  var resultado = jsonDecode(response.body);
+  var resultado = json.decode(utf8.decode(response.bodyBytes));
 
   return resultado;
 }
