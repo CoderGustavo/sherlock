77447407b91489eb7630d2cbd10e2a4072fb4c1d
Gustavo Ornaghi Antunes <gustavoornaghiantunes@gmail.com>
2024-10-31 18:55:31 -0300

    Get memory and cpu available in logs

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.add_middleware(ResourceMonitorMiddleware)
+
 app.add_middleware(LoggingMiddleware)
 app.add_middleware(ResourceMonitorMiddleware)
 
--- middlewares/monitor_middleware.py
+++ middlewares/monitor_middleware.py
@@ -19,6 +19,10 @@ class ResourceMonitorMiddleware(BaseHTTPMiddleware):
         cpu_start = self._get_cpu_usage()
         ram_start = process.memory_info().rss / (1024 * 1024)  # Convertendo para MB
 
+        # Captura o total disponível de CPU e RAM
+        total_cpu_available = 100 - cpu_start  # Total de CPU disponível em porcentagem
+        total_ram_available = psutil.virtual_memory().available / (1024 * 1024)  # RAM disponível em MB
+
         # Tempo de início
         start_time = time.time()
 
@@ -38,12 +42,14 @@ class ResourceMonitorMiddleware(BaseHTTPMiddleware):
 
         # Loga as informações
         logger.info(f"Tempo de processamento para {request.url.path}: {process_time:.4f} segundos")
-        logger.info(f"Uso de CPU: {cpu_usage:.2f}%")
-        logger.info(f"Uso de RAM: {ram_usage:.2f} MB")
+        logger.info(f"Uso de CPU: {cpu_usage:.2f}%, Total de CPU disponível: {total_cpu_available:.2f}%")
+        logger.info(f"Uso de RAM: {ram_usage:.2f} MB, Total de RAM disponível: {total_ram_available:.2f} MB")
 
         # Adiciona as informações nos headers da resposta (opcional)
         response.headers["X-Process-Time"] = str(process_time)
+        response.headers["X-Total-CPU-Available"] = f"{total_cpu_available:.2f}%"
         response.headers["X-CPU-Usage"] = f"{cpu_usage:.2f}%"
+        response.headers["X-Total-RAM-Available"] = f"{total_ram_available:.2f} MB"
         response.headers["X-RAM-Usage"] = f"{ram_usage:.2f} MB"
 
         return response
